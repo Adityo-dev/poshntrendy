40ed29554aa1121d5abccfe9b69078cec991a5fc
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-05-23 08:36:15 +0600

    Loading Add

--- src/components/modules/home/NewArrivals.js
+++ src/components/modules/home/NewArrivals.js
@@ -1,8 +1,4 @@
-import Categories from "@/components/modules/home/Categories";
-import FestiveCollections from "@/components/modules/home/FestiveCollections";
-import Hero from "@/components/modules/home/hero/Hero";
-import NewArrivals from "@/components/modules/home/NewArrivals";
-import SalesOngoing from "@/components/modules/home/SalesOngoing";
+import CollectionShowcase from "@/components/collectionShowcase/CollectionShowcase";
 
 const designerOnDiscount = [
   {
@@ -142,74 +138,20 @@ const designerOnDiscount = [
   },
 ];
 
-const weddingTales = [
-  {
-    id: 1,
-    image: "/image/product11.jpg",
-    title: "AWIGNA by Warsha & Rittu",
-    price: 471,
-    discount: 0,
-    rating: 5,
-    url: "awigna-by-warsha-ritt",
-  },
-  {
-    id: 2,
-    image: "/image/product10.jpg",
-    title: "Golden Glam Lehenga",
-    price: 432,
-    discount: 0,
-    rating: 4.9,
-    url: "golden-glam-lehenga",
-  },
-  {
-    id: 3,
-    image: "/image/product9.jpg",
-    title: "Royal Red Bridal Set",
-    price: 468,
-    discount: 10,
-    rating: 4.8,
-    url: "royal-red-bridal-set",
-  },
-  {
-    id: 4,
-    image: "/image/product3.jpg",
-    title: "Peach Elegance Embroidery",
-    price: 414,
-    discount: 10,
-    rating: 4.7,
-    url: "peach-elegance-embroidery",
-  },
-  {
-    id: 5,
-    image: "/image/product7.jpg",
-    title: "Maroon Net Bridal Wear",
-    price: 468,
-    discount: 10,
-    rating: 4.9,
-    url: "maroon-net-bridal-wear",
-  },
-  {
-    id: 6,
-    image: "/image/product6.jpg",
-    title: "Ivory Dream Lehenga",
-    price: 441,
-    discount: 10,
-    rating: 4.8,
-    url: "ivory-dream-lehenga",
-  },
-];
-
-export default function Home() {
+function NewArrivals() {
   return (
-    <main>
-      <Hero />
-      <Categories />
-      <NewArrivals />
-      <FestiveCollections />
-      {/* <BestSellers /> */}
-      {/* <ClientStories /> */}
-      <SalesOngoing />
-      {/* BRANDS */}
-    </main>
+    <CollectionShowcase
+      isGradient={"bg-peach-pink-gradient"}
+      title={"new arrivals"}
+      viewBtnText="Explore all"
+      description={
+        "Blending the richness of tradition with the innovation of modernity creates a unique tapestry where the past and present coexist in perfect harmony. In every corner of design, culture, fashion, and architecture."
+      }
+      products={designerOnDiscount}
+      isSimpleSlider={true}
+      compoName={"newArrivals"}
+    />
   );
 }
+
+export default NewArrivals;
