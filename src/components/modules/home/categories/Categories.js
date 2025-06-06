0b822893045b9509432d6baeedc221409d633159
Aditto dev Barmon (koushik) <134207015+Adityo-dev@users.noreply.github.com>
2025-06-07 02:12:52 +0600

    Client Stori Add and checkout add and other problem solve

--- src/components/modules/home/categories/Categories.js
+++ src/components/modules/home/categories/Categories.js
@@ -1,5 +1,5 @@
-import Image from "next/image";
 import Link from "next/link";
+import CategoriesCart from "./CategoriesCart";
 
 // data
 const categories = [
@@ -47,25 +47,7 @@ function Categories() {
 
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {categories.map((cate) => (
-          <Link
-            href={`/shop?filter=WoMen`}
-            key={cate?.id}
-            className="relative group overflow-hidden rounded-2xl"
-          >
-            <Image
-              src={cate?.image}
-              width={400}
-              height={400}
-              alt={cate?.title}
-              className="w-full h-[420px] object-cover transition-transform duration-300 group-hover:scale-110"
-            />
-            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black/80 to-transparent py-10 text-white text-center">
-              <p className="text-xl font-semibold">{cate?.title}</p>
-              <button className="mt-2 text-sm font-medium uppercase  ">
-                Shop now
-              </button>
-            </div>
-          </Link>
+          <CategoriesCart cate={cate} key={cate?.id} />
         ))}
       </div>
     </section>
--- src/components/modules/home/clientStories/ReviewCart.js
+++ src/components/modules/home/clientStories/ReviewCart.js
@@ -2,7 +2,8 @@ import Image from "next/image";
 
 function ReviewCart({ item }) {
   return (
-    <div className="relative bg-[#113955] px-4 sm:p-3 rounded-lg w-[350px] sm:w-[450px] h-52 sm:h-60 shadow-lg flex gap-4 cursor-pointer">
+    <div className="relative bg-[#FFF5F5] border hover:border-primary p-3 rounded-lg w-[450px] h-60 shadow-lg flex gap-4 cursor-pointer transition-all duration-200">
+      {/* Review Product Image */}
       <Image
         src={item?.image}
         width={400}
@@ -10,50 +11,24 @@ function ReviewCart({ item }) {
         alt={item?.name}
         className="w-[150px] h-full rounded-md object-cover"
       />
-      <div>
-        {/* Review Content */}
-        <div className="relative space-y-2">
-          <p className="text-[#0D99FF] sm:text-lg font-medium">{item?.date}</p>
-          <p className="text-[#F7FDFF] text-sm sm:text-base sm:leading-6">
-            {item?.description}
-          </p>
+      <div className="flex flex-col justify-between">
+        <div className="relative space-y-1">
+          <p className="text-[#FF69B4] text-lg font-medium">{item?.date}</p>
+          <p className="text-[#333333]">{item?.description}</p>
         </div>
         {/* Reviewer Info */}
-        <div className="flex items-center gap-4 mt-4">
-          <div className="space-y-0.5 sm:space-y-1">
-            <p className="text-sm sm:text-base sm:font-semibold text-white">
-              {item?.name}
-            </p>
-            <p className="text-gray-300 text-xs sm:text-sm">
-              {item?.designation}
-            </p>
-            <div className="flex items-center">
-              {Array.from({ length: item?.rating }, (_, i) => (
-                <span key={i} className="text-yellow-400 text-xs sm:text-sm">
-                  ⭐
-                </span>
-              ))}
-            </div>
+        <div className="space-y-0.5 sm:space-y-1">
+          <p className="text-base sm:font-semibold text-[#333333]">
+            Name: {item?.name}
+          </p>
+          <div className="flex items-center">
+            {Array.from({ length: item?.rating }, (_, i) => (
+              <span key={i} className="text-yellow-400 text-sm">
+                ⭐
+              </span>
+            ))}
           </div>
         </div>
-      </div>
-
-      {/* Quotation Icon */}
-      <div className="absolute bottom-6 right-6 flex gap-1">
-        {/* <Image
-          src="/image/inverted-comma.png"
-          width={16}
-          height={28}
-          alt="Quote"
-          className="w-4 sm:w-6 h-6 sm:h-10"
-        />
-        <Image
-          src="/image/inverted-comma.png"
-          width={16}
-          height={28}
-          alt="Quote"
-          className="w-4 sm:w-6 h-6 sm:h-10"
-        /> */}
       </div>
     </div>
   );
